394e8e3837b45fd485f152d54646896a2b2b249b
Alisa kh | Cyb3rKat <khodadadi.alisa@gmail.com>
2023-07-31 23:26:06 +0330

    fixing duplicate props

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} className='scroll-smooth  scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-thin'>{children}</body>
+      <body className={`${inter.className} scroll-smooth  scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-thin`}>{children}</body>
     </html>
   )
 }

c08dade047215ee542a52588b11b421ce7ae9c82
Alisa kh | Cyb3rKat <khodadadi.alisa@gmail.com>
2023-08-16 13:15:42 +0330

    minor changes

--- src/components/react/ContactPage.jsx
+++ src/components/react/ContactPage.jsx
@@ -20,12 +20,10 @@ const ContactPage = () => {
                     </div>
 
                     <div className={`h-full space-y-2 text-left  md:min-w-[500px] mt-10 w-full border-2 border-jamali-white rounded-xl px-2 py-10 sm:p-10`}>
-                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Tel:</span>981234566</p>
+                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Tel:</span>+98 912 430 3631</p>
                         <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Email:</span>info@ShahriyarJamali.art</p>
                         <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Instagram:</span><a className='text-jamali-white text-sm' href='https://www.instagram.com/shahriyarjamali_artworks/'>@shahriyarjamali_artworks</a></p>
-                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Whatapp:</span> +981234566</p>
-                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>pintrest:</span> https://www.pinterest.com/shahriyarjamaliartworks/</p>
-                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>dribbble:</span> https://dribbble.com/ShahriyarJamali</p>
+                        <p className='text-jamali-white text-sm'><span className='text-white mr-4'>dribbble:</span> dribbble.com/ShahriyarJamali</p>
                         <p className='text-jamali-white text-sm'><span className='text-white mr-4'>Website:</span>www.ShahriyarJamali.art</p>
                     </div>
                 </div>

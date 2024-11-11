d8384fb533cdaa71d9d97b5ebbd1e5431c398317
root <khodadadi.alisa@gmail.com>
2024-11-11 06:50:48 +0000

    social fixed

--- src/components/react/SideMenu.jsx
+++ src/components/react/SideMenu.jsx
@@ -13,10 +13,9 @@ const SideMenu = () => {
             <Link href='/' className=" h-48 vertical-rl w-full -rotate-180  flex items-center text-xl md:text-2xl text-jamali-black ">Shahriyar Jamali</Link>
             <div className=" grow"></div>
             <div className=" h-24 flex flex-col items-center justify-center w-full space-y-5">
-                <a href="tel:+989124303631" ><Image alt='Call' src={callIcon} width={22} height={22} /></a>
-                <a href='https://www.instagram.com/shahriyarjamali_artworks/' target='_blank'><Image alt='instagram' src={instagramIcon} /></a>
+                <a href='https://www.instagram.com/shahriyarjamali.art/' target='_blank'><Image alt='instagram' src={instagramIcon} /></a>
 
-                <a href='https://www.pinterest.com/shahriyarjamaliartworks/' target='_blank'><Image alt='pintrest' src={pintrestIcon} /></a>
+                <a href='https://www.pinterest.com/shahriyarjamaliart/' target='_blank'><Image alt='pintrest' src={pintrestIcon} /></a>
                 <a href='https://dribbble.com/ShahriyarJamali' target='_blank'><Image alt='dribble' src={dribbbleIcon} /></a>
             </div>
         </div>
